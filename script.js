0a718071e09b3293b7b3f33895e09fffda37cc22
maksimsavi <savianokm@gmail.com>
2020-06-01 01:47:20 -0500

    cross origin failure, need help

---
+++ script.js
@@ -0,0 +1,70 @@
+
+function getDogImage(dog_amount) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${dog_amount}`)
+      .then(response => response.json())
+      .then(responseJson => generateImgHTML(responseJson, dog_amount));
+      //.then(responseJson => generateImgHTML(responseJson));
+      
+      //generateImgHTML();
+
+  }
+function decideSubhead (dog_amount) {
+  if (dog_amount <2) {
+    return 'Look at this dog!'
+  } else {
+    return 'Look at these dogs!'
+  }
+}
+function generateImgHTML (responceJson, forminput) {
+  $(".dog_pics").replaceWith(`<div class="dog_pics">
+  <h2>${decideSubhead(forminput)}</h2>
+  </div>`);  
+  responceJson.message.forEach(element => {
+  console.log(element);
+    $("div.dog_pics").append(`<img src="${element}" alt="dog pic">`);    
+    });
+    
+    console.log('generate ran');
+}  
+
+function watchForm() {
+    $('.numberedRandomPics').submit(event => {
+      event.preventDefault();
+      var dog_amount = $( "input[type=text][name=dog_amount]" ).val();
+      getDogImage(dog_amount);
+    });
+  }
+  //random by breed
+    function watchBreedForm() {
+    $('.randomBreedPic').submit(event => {
+      event.preventDefault();
+      var dog_breed = $( "input[type=text][name=dog_breed]" ).val();
+      getDogImageByBreed(dog_breed);
+    });
+  } 
+  function getDogImageByBreed(dog_breed) {
+    fetch(`https://dog.ceo/api/breed/${dog_breed}/images/`)
+      .then(response => response.json())
+      .then(responceJson => console.log(responceJson))
+      .then(responseJson=> generateImgHTMLifRandom(responseJson, dog_breed));
+    }
+    function generateImgHTMLifRandom (responceJson, forminput) {
+      $(".dog_pics").replaceWith(`<div class="dog_pics">
+      <h2>'Look at this ${dog_breed}!'</h2>
+      </div>`);  
+      
+      $("div.dog_pics").append(`<img src="${responceJson.message}" alt="dog pic">`);
+        
+        console.log('generate breed ran');
+    }  
+$(function() {
+    console.log('started');
+    watchForm();
+    watchBreedForm();
+  });
+
+  //i have array with links.
+  //next i have to put in images.
+  //to do that I have to have a div probably that contains images.
+  //generate <img> and replace src="--" with the link from array.
+  //for each item in array I can append "<img src="${}">" 
