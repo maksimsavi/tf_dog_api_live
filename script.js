269e4b261208db47617bbfb4a013eaa1c5a61276
maksimsavi <savianokm@gmail.com>
2020-06-02 19:09:27 -0500

    final

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function getDogImage(dog_amount) {
     fetch(`https://dog.ceo/api/breeds/image/random/${dog_amount}`)
-      .then(response => response.json())
-      .then(responseJson => generateImgHTML(responseJson, dog_amount));
-      //.then(responseJson => generateImgHTML(responseJson));
-      abc = 'max';
-      //generateImgHTML();
-
+    .then(res => {
+      if (!res.ok) {
+          return res.json().then(json => {
+              throw new Error(json.message);
+          });
+      }
+  
+      return res.json();
+  })
+      .then(responseJson => generateImgHTML(responseJson, dog_amount))
+      .catch(error => alert(error));
   }
 function decideSubhead (dog_amount) {
   if (dog_amount <2) {
@@ -19,7 +24,6 @@ function generateImgHTML (responceJson, forminput) {
   <h2>${decideSubhead(forminput)}</h2>
   </div>`);  
   responceJson.message.forEach(element => {
-  console.log(element);
     $("div.dog_pics").append(`<img src="${element}" alt="dog pic">`);    
     });
     
@@ -29,7 +33,7 @@ function generateImgHTML (responceJson, forminput) {
 function watchForm() {
     $('.numberedRandomPics').submit(event => {
       event.preventDefault();
-      var dog_amount = $( "input[type=text][name=dog_amount]" ).val();
+      var dog_amount = $( "input[type=number][name=dog_amount]" ).val();
       getDogImage(dog_amount);
     });
   }
@@ -37,21 +41,28 @@ function watchForm() {
     function watchBreedForm() {
     $('.randomBreedPic').submit(event => {
       event.preventDefault();
-      var dog_breed = $( "input[type=text][name=dog_breed]" ).val();
+      var dog_breed = $( "input[type=text][name=dog_breed]" ).val().toLowerCase();
       getDogImageByBreed(dog_breed);
     });
   } 
   function getDogImageByBreed(dog_breed) {
     fetch(`https://dog.ceo/api/breed/${dog_breed}/images/random`)
-      .then(response => response.json())
+    .then(res => {
+      if (!res.ok) {
+          return res.json().then(json => {
+              throw new Error(json.message);
+          });
+      }
+  
+      return res.json();
+  })
       .then(responceJson=> generateImgHTMLifRandom(responceJson, dog_breed))
-      .catch(error => alert('Something went wrong. Try again later.'));
+      .catch(error => alert(error));
     }
     function generateImgHTMLifRandom (responceJson, dog_breed) {
       $(".dog_pics").replaceWith(`<div class="dog_pics">
       <h2>Look at this ${dog_breed}!</h2>
       </div>`);  
-      console.log(`${responceJson}`+'consoleteset');
       $("div.dog_pics").append(`<img src="${responceJson.message}" alt="dog pic">`);
         
         console.log('generate breed ran');
